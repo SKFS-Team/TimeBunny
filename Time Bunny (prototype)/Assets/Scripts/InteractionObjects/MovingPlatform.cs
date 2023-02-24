f54c9ed8bbd269c04dcd2481692bb8f0aa752b1b
Sanutro <sashabulochka82@gmail.com>
2023-02-24 17:13:42 +0200

    + MovingPlatform+ ButtonPLatform
    MovengPlatform have some bug, ButtonPlatform works when there is something on it

---
+++ Time Bunny (prototype)/Assets/Scripts/InteractionObjects/MovingPlatform.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MovingPlatform : MonoBehaviour
+{
+    private bool playerOnPlatform = false;
+    public float speed = 5;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerOnPlatform = true;
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
+            Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized * speed * Time.deltaTime;
+            transform.Translate(movement);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerOnPlatform = false;
+        }
+    }
+
+}
---
+++ Time Bunny (prototype)/Assets/Scripts/InteractionObjects/PlatformButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformButton : MonoBehaviour
+{
+    [SerializeField] private Animator OpenGameObject;
+    private Renderer rend;
+    private Color startColor;
+    [SerializeField] private Color HoverColor;
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        OpenGameObject.SetBool("Open", true);
+        rend.material.color = HoverColor;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        OpenGameObject.SetBool("Open", false);
+        rend.material.color = startColor;
+    }
+}
