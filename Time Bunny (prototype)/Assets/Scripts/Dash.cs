af22d4580e90764f7900e9c1ffb0165c2f2644a0
Sanutro <sashabulochka82@gmail.com>
2023-02-24 15:24:11 +0200

    Dash

---
+++ Time Bunny (prototype)/Assets/Scripts/Dash.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    public Transform c ;
+    private float Speed = 25;
+    private bool canDash = true;
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(2) && canDash && Camera.main.transform.rotation.x < 0 /*&& Camera.main.transform.rotation.x < -15*/)
+        {
+            canDash = false;
+            Invoke("CanDash", 1f);
+            rb.AddForce(Camera.main.transform.forward * Speed, ForceMode.Impulse);
+        }
+    }
+
+    void CanDash()
+    {
+        canDash = true;
+    }
+}
+
+
+
+
+
+
