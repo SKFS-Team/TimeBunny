7181ae4fe7fdc463518abea879a4b5e634433ba6
Sanutro <sashabulochka82@gmail.com>
2023-02-25 20:39:41 +0200

    StartDashAnim

--- Time Bunny (prototype)/Assets/Scripts/Player/Dash.cs
+++ Time Bunny (prototype)/Assets/Scripts/Player/Dash.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
-
 public class Dash : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private Animator CameraAnimator;
     public float Speed;
     private bool canDash = true;
     private void Update()
     {
-        if(Input.GetMouseButtonDown(2) && canDash /*&& Camera.main.transform.rotation.x < 0 /*&& Camera.main.transform.rotation.x < -15*/)
+        if(Input.GetMouseButtonDown(2) && canDash)
         {
             canDash = false;
+            CameraAnimator.SetTrigger("InDash");
             Invoke("CanDash", 1f);
             rb.AddForce(Vector3.Normalize(new Vector3(rb.velocity.x, 0, rb.velocity.z)) * Speed, ForceMode.Impulse);
         }
