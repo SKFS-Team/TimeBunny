c14b8ba4d436ac675db4135c0263acc0de7572cd
Sanutro <sashabulochka82@gmail.com>
2023-03-18 19:10:14 +0200

    FixedJumpPad

--- Time Bunny (prototype)/Assets/Scripts/InteractionObjects/JumpPad.cs
+++ Time Bunny (prototype)/Assets/Scripts/InteractionObjects/JumpPad.cs
@@ -2,25 +2,14 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour
 {
     [SerializeField] private Rigidbody playerRb;
-    private float force;
     [SerializeField] private float forceUp;
     [SerializeField] private float forceForward;
-    public bool NeedUp = false;
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
         {
-            switch (NeedUp)
-            {
-                case true:
-                    force = forceUp;
-                    playerRb.AddForce(Vector3.up * force, ForceMode.Impulse);
-                    break;
-                case false:
-                    force = forceForward;
-                    playerRb.AddForce(Vector3.forward * force, ForceMode.Impulse);
-                    break;
-            }
+            playerRb.AddForce(gameObject.transform.forward * forceForward * Time.deltaTime, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * forceUp * Time.deltaTime, ForceMode.Impulse);
         }  
     }
 }
